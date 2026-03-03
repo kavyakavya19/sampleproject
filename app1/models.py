5358d93e57959bbea7aceeae45e5fbd39c383cea
kavya <kavyavarsha19@gmail.com>
2026-03-03 09:51:55 +0530

    second commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+class Student(models.Model):
+    stud_id = models.CharField(max_length=10, primary_key=True)
+    stud_name = models.CharField(max_length=100)
+    reg_no = models.CharField(max_length=20, unique=True)
+    dep_name = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return self.stud_name
+    
+class Project(models.Model):
+    project_id = models.CharField(max_length=10, primary_key=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    project_title = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    coordinator_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.project_title
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.team_name
+    
+class Task(models.Model):
+    task_id = models.CharField(max_length=10, primary_key=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    project_title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_id
+    
+class Assignment(models.Model):
+    assignment_id = models.CharField(max_length=10, primary_key=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    assigned_date = models.DateField()
+
+    def __str__(self):
+        return self.assignment_id
+    
+class Status(models.Model):
+    status_id = models.CharField(max_length=10, primary_key=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    updated_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.task.task_id} - {self.status}"
--- todoapp/settings.py
+++ todoapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,13 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app1_db',
+        'USER': 'root',
+        'PASSWORD': '19Kavya@2005',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
